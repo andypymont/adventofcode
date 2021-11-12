e6502143df112792435fa5d7d22db9a0adc69aa7
Andy Pymont <andypymont@gmail.com>
2021-11-12 14:40:36 +0000

    2015 Day 1

---
+++ 2015/day01.py
@@ -0,0 +1,58 @@
+"""
+2015 Day 1
+https://adventofcode.com/2015/day/1
+"""
+
+import aocd # type: ignore
+
+def floor_reached(directions: str) -> int:
+    """
+    Calculate floor reached:
+     ( = up one floor
+     ) = down one floor
+    """
+    return sum(1 if char == '(' else -1 for char in directions)
+
+def test_part1():
+    """
+    Examples for Part 1.
+    """
+    assert floor_reached('(())') == 0
+    assert floor_reached('()()') == 0
+    assert floor_reached('(((') == 3
+    assert floor_reached('(()(()(') == 3
+    assert floor_reached('))(((((') == 3
+    assert floor_reached('())') == -1
+    assert floor_reached('))(') == -1
+    assert floor_reached(')))') == -3
+    assert floor_reached(')())())') == -3
+
+def enters_basement(directions: str) -> int:
+    """
+    On which instruction number (first instruction is 1) does Santa first enter
+    the basement?
+    """
+    total: int = 0
+
+    for i, direction in enumerate(directions):
+        total += (1 if direction == '(' else -1)
+        if total < 0:
+            return i + 1
+
+    return -1
+
+def test_part2():
+    """
+    Examples for Part 2.
+    """
+    assert enters_basement(')') == 1
+    assert enters_basement('()())') == 5
+
+if __name__ == '__main__':
+    data = aocd.get_data(year=2015, day=1)
+
+    p1 = floor_reached(data)
+    p2 = enters_basement(data)
+
+    print(f'Part 1: {p1}')
+    print(f'Part 2: {p2}')
--- template.py
+++ template.py
@@ -3,8 +3,13 @@ YYYY Day D
 https://adventofcode.com/YYYY/day/D
 """
 
-import aocd
+import aocd # type: ignore
 
 if __name__ == '__main__':
-    data: str = aocd.get_data(2015, 1)
-    print(data)
+    data = aocd.get_data(year=0, day=0)
+
+    p1 = len(data)
+    p2 = len(data)
+
+    print(f'Part 1: {p1}')
+    print(f'Part 2: {p2}')
