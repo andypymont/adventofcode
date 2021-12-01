a8a36ff61cd1fdfbdb62374633cc3c3689335f6d
Andy Pymont <andypymont@gmail.com>
2021-12-01 11:13:43 +0000

    2021 Day 1

---
+++ 2021/day01.py
@@ -0,0 +1,59 @@
+"""
+2021 Day 1
+https://adventofcode.com/2021/day/1
+"""
+
+from itertools import tee
+from operator import sub
+from typing import Any, Iterable, Tuple
+import aocd # type: ignore
+
+def pairwise(iterable: Iterable[Any]) -> Iterable[Tuple[Any, Any]]:
+    """
+    Pairwise (as implemented in itertools in Python 3.10)
+    e.g. pairwise('ABCDEFG') --> AB BC CD DE EF FG
+    """
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+def triwise(iterable: Iterable[Any]) -> Iterable[Tuple[Any, Any]]:
+    """
+    Three-variable version of pairwise.
+    e.g. triwise('ABCDEFG') --> ABC BCD CDE DEF EFG
+    """
+    a, b, c = tee(iterable, 3)
+    next(b, None)
+    next(c, None)
+    next(c, None)
+    return zip(a, b, c)
+
+def test_part1() -> None:
+    """
+    Examples for Part 1.
+    """
+    items = (199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
+    assert sum(1 for first, second in pairwise(items) if second > first) == 7
+
+def test_part2() -> None:
+    """
+    Examples for Part 2.
+    """
+    items = (199, 200, 208, 210, 200, 207)
+    triples = list(triwise(items))
+    assert triples == [(199, 200, 208), (200, 208, 210), (208, 210, 200), (210, 200, 207)]
+
+def main() -> None:
+    """
+    Calculate and output the solutions based on the real puzzle input.
+    """
+    data = aocd.get_data(year=2021, day=1)
+    numbers = [int(line) for line in data.split('\n')]
+
+    print(f'Part 1: {sum(1 for first, second in pairwise(numbers) if second > first)}')
+
+    windows = [sum(window) for window in triwise(numbers)]
+    print(f'Part 2: {sum(1 for first, second in pairwise(windows) if second > first)}')
+
+if __name__ == '__main__':
+    main()
