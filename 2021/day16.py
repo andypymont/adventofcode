bf4068af2b18cae86583573e245a3a696147221d
Andy Pymont <andypymont@gmail.com>
2021-12-16 15:47:58 +0000

    2021 Day 16: Tidied up Packet class to make typing clearer and avoid Union value

--- 2021/day16.py
+++ 2021/day16.py
@@ -4,8 +4,9 @@ https://adventofcode.com/2021/day/16
 """
 
 from dataclasses import dataclass
-from math import prod
-from typing import List, Tuple, Union
+from functools import reduce
+from operator import add, mul
+from typing import Callable, Dict, List, Tuple
 import aocd # type: ignore
 
 def decode_char(char: str) -> str:
@@ -16,13 +17,22 @@ def decode_char(char: str) -> str:
 def decode(text: str) -> str:
     return ''.join(decode_char(char) for char in text)
 
-PacketValue = Union[int, Tuple['Packet', ...]]
+OPERATIONS: Dict[int, Callable[[int, int], int]] = {
+    0: add,
+    1: mul,
+    2: min,
+    3: max,
+    5: lambda a, b: 1 if a > b else 0,
+    6: lambda a, b: 1 if a < b else 0,
+    7: lambda a, b: 1 if a == b else 0,
+}
 
 @dataclass(frozen=True)
 class Packet:
     version: int
     type_id: int
-    value: PacketValue
+    value: int
+    children: Tuple['Packet', ...]
 
     @classmethod
     def from_binary(cls, binary: str) -> 'Packet':
@@ -33,25 +43,15 @@ class Packet:
         return cls.from_binary(decode(hexadecimal))
 
     def calculate(self) -> int:
-        if isinstance(self.value, int):
+        if self.type_id == 4:
             return self.value
-        return {
-            0: sum,
-            1: prod,
-            2: min,
-            3: max,
-            5: lambda vals: 1 if vals[0] > vals[1] else 0,
-            6: lambda vals: 1 if vals[0] < vals[1] else 0,
-            7: lambda vals: 1 if vals[0] == vals[1] else 0,
-        }[self.type_id]([child.calculate() for child in self.value])
+        return reduce(OPERATIONS[self.type_id], self.children_values())
 
-    def children_version_sum(self) -> int:
-        if isinstance(self.value, int):
-            return 0
-        return sum(subpacket.version_sum() for subpacket in self.value)
+    def children_values(self) -> Tuple[int, ...]:
+        return tuple(child.calculate() for child in self.children)
 
     def version_sum(self) -> int:
-        return self.version + self.children_version_sum()
+        return self.version + sum(child.version_sum() for child in self.children)
 
 class BinaryDataReader:
 
@@ -96,19 +96,17 @@ class BinaryDataReader:
     def read_packet(self) -> Packet:
         version = self.read_number(3)
         type_id = self.read_number(3)
-        value: PacketValue = 0
-        if type_id == 4:
-            value = self.read_literal()
-        else:
-            length_type = self.read_digit()
-            if length_type == '0': # length type 0 = 15-bit length, length is # of bits
-                length = self.read_number(15)
-                value = self.read_subpackets_by_length(length)
-            else: # length type 1 = 11-bit length, length is # of sub-packets
-                length = self.read_number(11)
-                value = self.read_subpackets_by_quantity(length)
+        children: Tuple[Packet, ...] = ()
 
-        return Packet(version, type_id, value)
+        if type_id == 4:
+            return Packet(version, type_id, self.read_literal(), children)
+
+        length_type = self.read_digit()
+        if length_type == '0': # length type 0 = 15-bit length, length is # of bits
+            children = self.read_subpackets_by_length(self.read_number(15))
+        else: # length type 1 = 11-bit length, length is # of sub-packets
+            children = self.read_subpackets_by_quantity(self.read_number(11))
+        return Packet(version, type_id, 0, children)
 
 def test_part1() -> None:
     """
@@ -119,13 +117,13 @@ def test_part1() -> None:
     assert decode('A9') == '10101001'
     assert decode('01') == '00000001'
     assert decode('D2FE28') == '110100101111111000101000'
-    assert Packet.from_binary('110100101111111000101000') == Packet(6, 4, 2021)
+    assert Packet.from_binary('110100101111111000101000') == Packet(6, 4, 2021, tuple())
     assert Packet.from_binary(
         '00111000000000000110111101000101001010010001001000000000'
-    ) == Packet(1, 6, (Packet(6, 4, 10), Packet(2, 4, 20)))
+    ) == Packet(1, 6, 0, (Packet(6, 4, 10, ()), Packet(2, 4, 20, ())))
     assert Packet.from_binary(
         '11101110000000001101010000001100100000100011000001100000'
-    ) == Packet(7, 3, (Packet(2, 4, 1), Packet(4, 4, 2), Packet(1, 4, 3)))
+    ) == Packet(7, 3, 0, (Packet(2, 4, 1, ()), Packet(4, 4, 2, ()), Packet(1, 4, 3, ())))
     assert Packet.from_hexadecimal('8A004A801A8002F478').version_sum() == 16
     assert Packet.from_hexadecimal('620080001611562C8802118E34').version_sum() == 12
     assert Packet.from_hexadecimal('C0015000016115A2E0802F182340').version_sum() == 23
